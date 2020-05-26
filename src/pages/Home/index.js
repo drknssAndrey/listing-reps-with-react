a9316ab8dd44d2ed207299b85d40206b2c3e996c
Andrey Balbino <andreymelo98@gmail.com>
2020-05-26 16:42:39 -0300

    Application with react

---
+++ src/pages/Home/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import axios from "axios";
+// import logo from "./logo.svg";
+// import "./App.css";
+import * as S from "./styled";
+import { useHistory } from "react-router-dom";
+
+function App(props) {
+  const history = useHistory();
+  const [usuario, setUsuario] = useState("");
+  const [erro, setErro] = useState(false);
+  function handlePesquisa() {
+    axios
+      .get(`https://api.github.com/users/${usuario}/repos`)
+      .then((response) => {
+        const repositories = response.data;
+        const repositoriesName = [];
+        repositories.map((repository) => {
+          repositoriesName.push(repository.name);
+        });
+        localStorage.setItem(
+          "repositoriesName",
+          JSON.stringify(repositoriesName)
+        );
+        setErro(false);
+        history.push("/repositories");
+      })
+      .catch((err) => {
+        setErro(true);
+      });
+  }
+  return (
+    <S.HomeContainer>
+      <S.Content>
+        <S.input
+          className="usuario "
+          placeholder="Usuario"
+          value={usuario}
+          onChange={(e) => setUsuario(e.target.value)}
+        ></S.input>
+        <S.button type="button" onClick={handlePesquisa}>
+          Pesquisar
+        </S.button>
+      </S.Content>
+      {erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente.</S.ErrorMsg> : ""}
+    </S.HomeContainer>
+  );
+}
+
+export default App;
